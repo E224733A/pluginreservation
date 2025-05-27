52c39eb3a4d519468ab0291deeb14ba1861d8b5b
E224733A <noebracq973@gmail.com>
2025-05-27 19:26:32 +0200

    loader sécurisé

--- includes/class-ponti-loader.php
+++ includes/class-ponti-loader.php
@@ -1,6 +1,5 @@
 <?php
 if (!defined('ABSPATH')) exit;
-
 class Ponti_Loader {
     private static function safe_require($path) {
         if (file_exists($path)) {
@@ -11,28 +10,21 @@ class Ponti_Loader {
     }
 
     public static function init() {
-        // CPT créneaux
         self::safe_require(PONTI_PLUGIN_PATH . 'includes/post-types/creneau.php');
-
-        // Shortcodes
         self::safe_require(PONTI_PLUGIN_PATH . 'public/shortcode-agenda.php');
-
-        // Admin uniquement
-        if (is_admin()) {
-            self::safe_require(PONTI_PLUGIN_PATH . 'admin/admin-menu.php');
-            self::safe_require(PONTI_PLUGIN_PATH . 'admin/meta-boxes/creneau-meta.php');
-            self::safe_require(PONTI_PLUGIN_PATH . 'admin/admin-abonnements.php');
-            self::safe_require(PONTI_PLUGIN_PATH . 'admin/export-reservations.php');
-        }
-
-        // WooCommerce + public
-        self::safe_require(PONTI_PLUGIN_PATH . 'woocommerce/credits-handler.php');
         self::safe_require(PONTI_PLUGIN_PATH . 'public/shortcode-mes-cours.php');
         self::safe_require(PONTI_PLUGIN_PATH . 'public/shortcode-mes-cours-coach.php');
         self::safe_require(PONTI_PLUGIN_PATH . 'public/credit-badge.php');
+        self::safe_require(PONTI_PLUGIN_PATH . 'woocommerce/credits-handler.php');
 
-        // Modules internes
         self::safe_require(PONTI_PLUGIN_PATH . 'includes/post-types/notifier.php');
         self::safe_require(PONTI_PLUGIN_PATH . 'includes/post-types/cleaner.php');
+
+        if (is_admin()) {
+            self::safe_require(PONTI_PLUGIN_PATH . 'admin/admin-menu.php');
+            self::safe_require(PONTI_PLUGIN_PATH . 'admin/meta-boxes/creneau-meta.php');
+            self::safe_require(PONTI_PLUGIN_PATH . 'admin/export-reservations.php');
+            self::safe_require(PONTI_PLUGIN_PATH . 'admin/admin-abonnements.php');
+        }
     }
 }

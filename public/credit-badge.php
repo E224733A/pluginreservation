b23676a445b4314c4661bbad7e8cfaa4e06e1b92
E224733A <noebracq973@gmail.com>
2025-05-28 17:38:58 +0200

    testcss

--- public/credit-badge.php
+++ public/credit-badge.php
@@ -1,6 +1,8 @@
 <?php
 if (!defined('ABSPATH')) exit;
 
+add_action('wp_enqueue_scripts', 'ponti_enqueue_styles', 99);
+
 add_action('astra_header_after', function () {
     if (!is_user_logged_in()) return;
 
